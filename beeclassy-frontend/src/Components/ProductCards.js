7c80d0edfbc3c14d9dcfb402066482229e855a5c
Ali <alitopbas0@hotmail.com>
2020-08-16 18:20:59 +0300

    Data started to be kept in a specific JS file. Components are mapped and the code is made more readable.

--- beeclassy-frontend/src/Components/ProductCards.js
+++ beeclassy-frontend/src/Components/ProductCards.js
@@ -1,86 +1,22 @@
 import React from 'react';
-import {
-  Card, Button, CardImg, CardTitle, CardText, CardGroup,
-  CardSubtitle, CardBody
-} from 'reactstrap';
+import { CardGroup } from 'reactstrap';
 import ProductCard from './ProductCard'
+import productData from './ProductData'
 
 const ProductCards = (props) => {
+
+  const productComponents = productData.map(product => <ProductCard 
+    key={product.id}
+    imgUrl={product.imgUrl}
+    alt={product.alt}
+    title={product.title}
+    subtitle={product.subtitle}
+    text={product.text}
+    button={product.button} />)
+
   return (
     <CardGroup className="cardGroupStyles">
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard> 
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
-    
-      <ProductCard 
-          imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-          alt="Card image cap"
-          title="Card title"
-          subtitle="Card subtitle"
-          text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-          button="Button"
-      >
-      </ProductCard>
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
-      <ProductCard 
-        imgUrl="https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg"
-        alt="Card image cap"
-        title="Card title"
-        subtitle="Card subtitle"
-        text="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-        button="Button"
-      >
-      </ProductCard>
+      {productComponents}
     </CardGroup>
     
   );
---
+++ beeclassy-frontend/src/Components/ProductData.js
@@ -0,0 +1,76 @@
+const productData = [
+    {
+        id:1,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:2,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:3,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:4,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:5,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:6,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:7,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+    {
+        id:8,
+        imgUrl:"https://statics.boyner.com.tr/contents/images/Landing/omuz-cantasi.jpg",
+        alt:"Card image cap",
+        title:"Card title",
+        subtitle:"Card subtitle",
+        text:"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+        button:"Button"
+    },
+]
+
+export default productData;
