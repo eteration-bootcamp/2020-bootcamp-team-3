c567a9c709f986690bda1036494215f82980898c
Ali <alitopbas0@hotmail.com>
2020-08-17 21:24:19 +0300

    AboutUs page and Routes file created

--- beeclassy-frontend/src/Components/Routes.js
+++ beeclassy-frontend/src/Components/Routes.js
@@ -1,16 +1,26 @@
-import React, { Component } from "react";
-import { Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-import Main from '../App'
+import Main from '../App';
+import AboutUs from '../Components/AboutUs';
+import NotFound from '../Components/';
+import Cart from "../Components/Cart";
 
-export default class Routes extends Component {
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Main} />
-                </Switch>
-            </Router>
-        )
-    }
+const Routes = () => {
+  return (
+    <Router>
+        <Switch>
+          <Route path="/about" component={AboutUs} />
+          <Route exact path="/" component={Main}/>
+          <Route path="/cart" component={Cart} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+    </Router>
+  );
 }
+
+export default Routes;
