d428d19773e74efc529c5bf500172f2ffc1f322d
Ali <alitopbas0@hotmail.com>
2020-08-17 18:57:48 +0300

    Carousel, Cart, NavBar parts are arranged. The deficiencies were fixed.

--- beeclassy-frontend/src/Components/ProductCard.js
+++ beeclassy-frontend/src/Components/ProductCard.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import {
-    Card, Button, CardImg, CardTitle, CardBody
-  } from 'reactstrap';
+import {Card, Button, CardImg, CardTitle, CardBody} from 'reactstrap';
 
 export default function ProductCard(props) {
     return (
